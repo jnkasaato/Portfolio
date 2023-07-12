6e19c1e694b256258524086d421adee9a140df65
jnkasaato <jnkasaato@gmail.com>
2023-07-12 01:55:30 -0400

    tab

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,8 +12,8 @@ return (
       <h4> SOLIDITY  -  ETHERS.JS  -  HARDHAT  -  REACT </h4>
       <h5>Seedling is a decentralized token exchange where you can transfer real world currency with a synthisized dApp token. You can deposit, withdraw, make and cancel orders, and access the ordering book all in one simple site.</h5>
       <div className = "projects__buttons">
-        <a href="seedling.jordananamutebi.com"><h1 >VISIT> </h1></a>
-        <a href="https://github.com/jnkasaato/coin-exchange"><h1>CODE> </h1></a>
+        <a href="https://seedling.jordananamutebi.com" target="_blank"><h1 >VISIT> </h1></a>
+        <a href="https://github.com/jnkasaato/coin-exchange" target="_blank"><h1>CODE> </h1></a>
       </div>
     </div>
     <div className = "projects__img">
@@ -30,8 +30,8 @@ return (
       <h4> SOLIDITY  -  ETHERS.JS  -  HARDHAT  -  REACT </h4>
       <h5>Acclimate is a decentrlalized ecomerce site where you can explore and buy anything you want in the same way you would on a centralized platform. </h5>
       <div className = "projects__buttons">          
-        <a href="www.acclimate.jordananamutebi.com"><h1 >VISIT> </h1></a>
-        <a href="https://github.com/jnkasaato/ecommerce"><h1>CODE> </h1></a>
+        <a href="https://acclimate.jordananamutebi.com"> target="_blank"<h1 >VISIT> </h1></a>
+        <a href="https://github.com/jnkasaato/ecommerce" target="_blank"><h1>CODE> </h1></a>
       </div>
     </div>  
   </div>
