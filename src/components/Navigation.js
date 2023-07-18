dfc47eb59946d946730c62e67b460c669bd8c370
jnkasaato <jnkasaato@gmail.com>
2023-07-18 14:37:32 -0400

    update link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,7 @@ function Navigation() {
 return (
   <div className = "nav">
     <img src = {jk} height = "80px"/>
-    <a href="#dapp">TOKEN EXCHANGE</a>
+    <a href="#dapp">SEEDLING</a>
     <a href="#ecommerce">ACCLIMATE</a>
     <a href="#contact" >CONTACT</a>
   </div>
