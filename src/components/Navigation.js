a4e592945395213b36f401de32a5baf9853c4840
jnkasaato <jnkasaato@gmail.com>
2023-07-28 21:37:19 -0400

    update scrolling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,14 +7,13 @@ function Navigation({ scrolling }) {
   return (
     <div className={`nav ${scrolling ? 'scrolling' : ''}`}>
       <img src={jk} height="80px" alt="Logo" />
-      {/* Use Link component from react-scroll for smooth scrolling */}
-      <Link to="dapp" smooth={true} duration={1000}>
+      <Link to="dapp" smooth={true} duration={1000} offset={-180}> 
         SEEDLING
       </Link>
-      <Link to="ecommerce" smooth={true} duration={1000}>
+      <Link to="ecommerce" smooth={true} duration={1000} offset={-180}>
         ACCLIMATE
       </Link>
-      <Link to="contact" smooth={true} duration={1000}>
+      <Link to="contact" smooth={true} duration={1000} offset={-180}>
         CONTACT
       </Link>
     </div>
@@ -22,3 +21,4 @@ function Navigation({ scrolling }) {
 }
 
 export default Navigation;
+
